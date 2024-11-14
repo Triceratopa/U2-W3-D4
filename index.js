7710c25a19fc68026346ab3b0d2b45fa0be9f27d
Giulia Gallo <giulia.gallo2903@gmail.com>
2024-11-14 16:59:13 +0100

    .

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const searchSecButton=document.getElementsByClassName('.btn-secondary')[0]
 
 
 const Api_key='UjEAwYw8tNHEocupamN4s1vSQJDPLQKEU2oyeK2RhJjkmk3bER4kbaR6'
-const getImg=function(){
+const getImg=function(query){
     fetch('https://api.pexels.com/v1/search?query=' + query,{
-        Headers:{
+        headers:{
         Authorization: Api_key}})
     .then((response)=>{
         if (response.ok){
@@ -15,7 +15,7 @@ const getImg=function(){
         }
     })
     .then((risultato)=>{
-     
+     console.log(risultato)
     })
     .catch((err)=> {
         console.log('no no',err)
